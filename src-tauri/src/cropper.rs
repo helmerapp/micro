1693435227260fcb3a05615e1493785a707c612b
Siddharth <siddharth99c@gmail.com>
2023-12-10 21:41:30 +0530

    feat: improves cropper

--- src-tauri/src/cropper.rs
+++ src-tauri/src/cropper.rs
@@ -1,17 +1,5 @@
 use tauri::{AppHandle, Manager, PhysicalSize, Position, Size, WindowBuilder, WindowUrl};
 
-#[cfg(target_os = "macos")]
-use tauri_nspanel::WindowExt;
-
-#[cfg(target_os = "macos")]
-use cocoa::base::nil;
-
-#[cfg(target_os = "macos")]
-use cocoa::foundation::NSString;
-
-#[cfg(target_os = "macos")]
-use objc::{class, msg_send, sel, sel_impl};
-
 pub fn init_cropper(app: &AppHandle) {
     // create cropper window
     let cropper_win = WindowBuilder::new(app, "cropper", WindowUrl::App("/cropper".into()))
@@ -39,26 +27,30 @@ pub fn init_cropper(app: &AppHandle) {
     cropper_win.set_position(pos).unwrap();
 
     #[cfg(target_os = "macos")]
-    cropper_win.to_panel().unwrap();
+    {
+        use cocoa::{appkit::NSColor, base::nil, foundation::NSString};
+        use objc::{class, msg_send, sel, sel_impl};
+        use tauri_nspanel::WindowExt;
 
-    #[cfg(target_os = "macos")]
-    cropper_win
+        cropper_win.to_panel().unwrap();
+        cropper_win
         .to_owned()
         .run_on_main_thread(move || unsafe {
             let id = cropper_win.ns_window().unwrap() as cocoa::base::id;
             let color =
-                cocoa::appkit::NSColor::colorWithSRGBRed_green_blue_alpha_(nil, 0.0, 0.0, 0.0, 0.0);
+                NSColor::colorWithSRGBRed_green_blue_alpha_(nil, 0.0, 0.0, 0.0, 0.0);
             let _: cocoa::base::id = msg_send![id, setBackgroundColor: color];
             cropper_win.with_webview(|webview| {
-								// !!! has delay
-								let id = webview.inner();
-								let no: cocoa::base::id = msg_send![class!(NSNumber), numberWithBool:0];
-								let _: cocoa::base::id =
-										msg_send![id, setValue:no forKey: NSString::alloc(nil).init_str("drawsBackground")];
-						})
-						.ok();
+                // !!! has delay
+                let id = webview.inner();
+                let no: cocoa::base::id = msg_send![class!(NSNumber), numberWithBool:0];
+                let _: cocoa::base::id =
+                        msg_send![id, setValue:no forKey: NSString::alloc(nil).init_str("drawsBackground")];
+        })
+        .ok();
         })
         .unwrap();
+    }
 }
 
 pub fn toggle_cropper(app: &AppHandle) {
