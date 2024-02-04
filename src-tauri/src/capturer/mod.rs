269d59f599b19556af1803595d4f6c9b7a234b69
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-02-04 12:56:49 +0530

    feat: poc for creating gif encoding

--- src-tauri/src/capturer/mod.rs
+++ src-tauri/src/capturer/mod.rs
@@ -1,9 +1,23 @@
 // use scap::capturer::{Capturer, Options};
 use crate::{AppState, Status};
-use std::sync::Arc;
+use std::thread;
 use helmer_media::encoder;
+use imgref::Img;
+use rgb::RGBA;
+use scap::frame;
 use tauri::{AppHandle, Manager};
 use tokio::sync::Mutex;
+
+fn transform_frame_bgr0(frame: frame::BGRFrame) -> Img<Vec<RGBA<u8>>> {
+    let frame_data = frame.data;
+    let mut rgba_data: Vec<RGBA<u8>> = Vec::with_capacity(frame_data.len() / 4);
+
+    for src in frame_data.chunks_exact(3) {
+        rgba_data.push(RGBA::new(src[0], src[1], src[2], 255))
+    }
+
+    Img::new(rgba_data, frame.width as usize, frame.height as usize)
+}
 
 #[tauri::command]
 pub async fn start_capture(area: Vec<u32>, app_handle: AppHandle) {
@@ -60,14 +74,67 @@ pub async fn start_capture(area: Vec<u32>, app_handle: AppHandle) {
     let mut frame_idx = 0;
     let mut frame_timestamp = helmer_media::Timestamp::new(frame_idx, time_base);
     println!("Encoding frames...");
-    for frame in frames {
+    for frame in frames.iter_mut() {
         encoder.ingest_next_video_frame(frame, frame_timestamp);
 
         frame_idx += 1;
         frame_timestamp = helmer_media::Timestamp::new(frame_idx, time_base);
     }
     encoder.done();
+    drop(encoder);
     println!("Encoding completed");
+
+    // Starting GIF creation
+    println!("Starting Gif creation");
+
+    let mut settings = gifski::Settings::default();
+    settings.repeat = gifski::Repeat::Infinite;
+
+    let mut no_progress = gifski::progress::NoProgress {};
+
+    let (gif_encoder, gif_writer) = gifski::new(settings).unwrap();
+
+    let gif = match std::fs::File::create("/Users/pranav2612000/Desktop/final.gif") {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("Error creating GIF file: {:?}", err);
+            return;
+        }
+    };
+
+    let handle = thread::spawn(move || {
+        println!("Writing to a GIF file");
+        let write_result = gif_writer.write(gif, &mut no_progress);
+        if let Err(err) = write_result {
+            eprintln!("Error writing GIF file: {:?}", err);
+        }
+        println!("Finished writing");
+    });
+
+    let mut i = 0;
+    println!("Encoding frames to gif");
+    for frame in frames {
+        match frame {
+            scap::frame::Frame::BGR0(bgr_frame) => {
+                let img = transform_frame_bgr0(bgr_frame);
+                gif_encoder
+                .add_frame_rgba(i, img, i as f64 * 0.5)
+                .unwrap_or_else(|err| {
+                    eprintln!("Error adding frame to encoder: {:?}", err);
+                });
+
+                i += 1;
+            },
+            _ => {
+                panic!("This frame type is not supported on BGR0 yet");
+            }
+        }
+    }
+    drop(gif_encoder);
+    println!("Encoding to gif completed");
+
+    handle.join();
+    println!("writing to a GIF completed");
 
     println!("complete"); // this never runs
 }
