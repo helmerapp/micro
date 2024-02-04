9c5d643d6e501949a54bc0229c3a3b15125a631c
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-02-04 11:41:47 +0530

    feat: poc for creating dummy video and saving it

--- src-tauri/src/capturer/mod.rs
+++ src-tauri/src/capturer/mod.rs
@@ -1,6 +1,7 @@
 // use scap::capturer::{Capturer, Options};
 use crate::{AppState, Status};
 use std::sync::Arc;
+use helmer_media::encoder;
 use tauri::{AppHandle, Manager};
 use tokio::sync::Mutex;
 
@@ -11,14 +12,63 @@ pub async fn start_capture(area: Vec<u32>, app_handle: AppHandle) {
     // TODO: initialize scap and start capturing
     println!("Cropped Area: {:?}", area);
 
-    // Update app state
-    let state_mutex = app_handle.state::<Mutex<AppState>>();
-    let mut state = state_mutex.lock().await;
-    state.status = Status::Recording;
+    const FRAME_TYPE:scap::frame::FrameType = scap::frame::FrameType::BGR0;
+    let options = scap::capturer::Options {
+        fps: 60,
+        targets: Vec::new(),
+        show_cursor: true,
+        show_highlight: true,
+        excluded_targets: None,
+        output_type: FRAME_TYPE,
+        output_resolution: scap::capturer::Resolution::_480p,
+        source_rect: Some(scap::capturer::CGRect {
+            origin: scap::capturer::CGPoint { x: 0.0, y: 0.0 },
+            size: scap::capturer::CGSize {
+                width: 600.0,
+                height: 400.0,
+            },
+        }),
+        ..Default::default()
+    };
+    let mut recorder = scap::capturer::Capturer::new(options);
+    recorder.start_capture();
 
-    // TEMP: stop capturing after 5 seconds, to get to editor
-    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-    stop_capture(app_handle.clone()).await;
+    println!("Capturing frames...");
+    let mut frames: Vec<scap::frame::Frame> = Vec::new();
+    for _ in 0..200 {
+        let frame = recorder.get_next_frame().expect("Error");
+        frames.push(frame);
+    }
+    recorder.start_capture();
+    println!("All frames captured");
+
+    let [output_width, output_height] = recorder.get_output_frame_size();
+
+    // Create Encoder
+    let mut encoder = encoder::Encoder::new(encoder::Options {
+        output: encoder::Output::FileOutput(encoder::FileOutput {
+            output_filename: "/Users/pranav2612000/Desktop/dummy.mp4".to_owned(),
+        }),
+        input: encoder::InputOptions {
+            width: output_width as usize,
+            height: output_height as usize,
+            frame_type: FRAME_TYPE,
+        },
+    });
+
+    let time_base = helmer_media::TimeBase::new(1, 25);
+    let mut frame_idx = 0;
+    let mut frame_timestamp = helmer_media::Timestamp::new(frame_idx, time_base);
+    println!("Encoding frames...");
+    for frame in frames {
+        encoder.ingest_next_video_frame(frame, frame_timestamp);
+
+        frame_idx += 1;
+        frame_timestamp = helmer_media::Timestamp::new(frame_idx, time_base);
+    }
+    encoder.done();
+    println!("Encoding completed");
+
     println!("complete"); // this never runs
 }
 
