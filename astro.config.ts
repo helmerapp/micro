27d3fa0b9bd55c0de8cc653d6c4d2d22246930f9
Siddharth <siddharth99c@gmail.com>
2024-02-04 08:54:21 +0530

    feat: set up tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   server: {
     port: 1420
   },
