c0e225b4773bb86edbc3836d00554e2791cab5cf
Siddharth <siddharth99c@gmail.com>
2024-02-04 20:15:43 +0530

    fix: typescript complains

--- src/components/editor/Preview.tsx
+++ src/components/editor/Preview.tsx
@@ -2,39 +2,35 @@ import { useState, useEffect, useRef } from "react";
 import { convertFileSrc } from '@tauri-apps/api/tauri'
 import * as Slider from '@radix-ui/react-slider';
 
-export default function Preview({ selectedFrames, setSelectedFrames }: {
+export default function Preview({
+	selectedFrames,
+	setSelectedFrames
+}: {
 	selectedFrames: number[],
 	setSelectedFrames: (e: number[]) => void
 }) {
+	const previewFps = 30; // Assume preview is 30 fps for now
+	const videoRef = useRef<HTMLVideoElement>(null) as React.MutableRefObject<HTMLVideoElement>;
 	const [totalFrames, setTotalFrames] = useState(0);
 
 
-	const previewRef = useRef<HTMLVideoElement>(null);
-
-	const previewFps = 30; // Assume 30 fps for now
-
 	const handleInput = (e: number[]) => {
 
-		// check which number is changing and update the video accordingly
+		// check which handle is changing and update preview
 		if (e[0] !== selectedFrames[0]) {
-			// @ts-expect-error
-			previewRef.current.currentTime = e[0] / previewFps;
+			videoRef.current.currentTime = e[0] / previewFps;
 		} else if (e[1] !== selectedFrames[1]) {
-			// @ts-expect-error
-			previewRef.current.currentTime = e[1] / previewFps;
+			videoRef.current.currentTime = e[1] / previewFps;
 		}
 
 		setSelectedFrames(e)
 	};
 
 	const handlePlayPause = () => {
-		// @ts-expect-error
-		if (previewRef.current.paused) {
-			// @ts-expect-error
-			previewRef.current.play();
+		if (videoRef.current.paused) {
+			videoRef.current.play();
 		} else {
-			// @ts-expect-error
-			previewRef.current.pause();
+			videoRef.current.pause();
 		}
 	}
 
@@ -44,16 +40,13 @@ export default function Preview({ selectedFrames, setSelectedFrames }: {
 		const previewPath = params.get("file");
 		const previewUrl = convertFileSrc(previewPath!);
 
-		// @ts-expect-error — Set the video source to the preview URL
-		previewRef.current.src = previewUrl;
-		// @ts-expect-error
-		previewRef.current.load();
+		// Set the source to the preview URL
+		videoRef.current.src = previewUrl;
+		videoRef.current.load();
 
-		// Get the duration (in seconds) after the video is loaded
-		// @ts-expect-error
-		previewRef.current.addEventListener('loadedmetadata', () => {
-			// @ts-expect-error
-			const duration = previewRef.current.duration;
+		// Get duration (in seconds) after the video is loaded
+		videoRef.current.addEventListener('loadedmetadata', () => {
+			const duration = videoRef.current.duration;
 			const frames = Math.floor(duration * previewFps);
 			setTotalFrames(frames);
 			setSelectedFrames([0, frames]);
@@ -63,16 +56,16 @@ export default function Preview({ selectedFrames, setSelectedFrames }: {
 
 	return <div className="w-[95%] h-fit rounded-xl overflow-hidden m-auto mt-0 mb-0 flex flex-col p-4 gap-4">
 		<video muted controls={false}
-			className="w-full object-cover rounded-md overflow-hidden h-[380px] border border-neutral-600" width={800} height={480} ref={previewRef}
+			className="w-full object-cover rounded-md overflow-hidden h-[380px] border border-neutral-600" width={800} height={480} ref={videoRef}
 			onClick={handlePlayPause}
 		/>
 
 		<Slider.Root
-			className="relative flex items-center select-none touch-none h-5"
-			value={selectedFrames}
 			min={0}
-			max={totalFrames}
 			step={1}
+			max={totalFrames}
+			value={selectedFrames}
+			className="relative flex items-center select-none touch-none h-5"
 			onValueChange={(e) => handleInput(e)}
 		>
 			<Slider.Track className="bg-black relative grow rounded-full h-[3px] w-full">
