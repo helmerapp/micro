7e34932630c80267b789fe1ef1eb2ffb6457e5f3
Siddharth <siddharth99c@gmail.com>
2024-02-08 09:49:32 +0530

    fix: window size and transparency on mac

--- src-tauri/src/toolbar/mod.rs
+++ src-tauri/src/toolbar/mod.rs
@@ -1,5 +1,6 @@
-use serde::{Deserialize, Serialize};
-use tauri::{App, AppHandle, Manager, PhysicalSize, Position, Size, WindowBuilder, WindowUrl};
+use tauri::{
+    AppHandle, LogicalSize, Manager, PhysicalSize, Position, Size, WindowBuilder, WindowUrl,
+};
 
 pub fn init_toolbar(app: &AppHandle) {
     println!("toolbar created");
@@ -11,22 +12,43 @@ pub fn init_toolbar(app: &AppHandle) {
         .focused(true)
         .center();
 
-
-    #[cfg(not(target_os = "macos"))]{
+    #[cfg(not(target_os = "macos"))]
+    {
         toolbar_win = toolbar_win.transparent(true);
     }
-    
 
     let toolbar_win = toolbar_win.build().expect("Failed to open toolbar");
 
-    let monitor = toolbar_win.primary_monitor().unwrap().unwrap();
-
-    let size = Size::Physical(PhysicalSize {
-        width: 150,
-        height: 70,
+    let size = Size::Logical(LogicalSize {
+        width: 150.0,
+        height: 70.0,
     });
 
     toolbar_win.set_size(size).unwrap();
+
+    #[cfg(target_os = "macos")]
+    {
+        use cocoa::{appkit::NSColor, base::nil, foundation::NSString};
+        use objc::{class, msg_send, sel, sel_impl};
+
+        toolbar_win
+        .to_owned()
+        .run_on_main_thread(move || unsafe {
+            let id = toolbar_win.ns_window().unwrap() as cocoa::base::id;
+            let color =
+                NSColor::colorWithSRGBRed_green_blue_alpha_(nil, 0.0, 0.0, 0.0, 0.0);
+            let _: cocoa::base::id = msg_send![id, setBackgroundColor: color];
+            toolbar_win.with_webview(|webview| {
+                // !!! has delay
+                let id = webview.inner();
+                let no: cocoa::base::id = msg_send![class!(NSNumber), numberWithBool:0];
+                let _: cocoa::base::id =
+                        msg_send![id, setValue:no forKey: NSString::alloc(nil).init_str("drawsBackground")];
+        })
+        .ok();
+        })
+        .unwrap();
+    }
 }
 
 // create a toggle_toolbar function
