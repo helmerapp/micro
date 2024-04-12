83aab1149991e2833d2c2355882c28f8b8d942a2
clearlysid <siddharth99c@gmail.com>
2024-04-12 08:05:51 +0530

    feat: remove slider and self-contain preview

--- src/components/editor/Preview.tsx
+++ src/components/editor/Preview.tsx
@@ -1,73 +1,21 @@
 import { useState, useEffect, useRef } from "react";
 import { convertFileSrc } from '@tauri-apps/api/core'
-import * as Slider from '@radix-ui/react-slider';
+import { listen } from '@tauri-apps/api/event';
+
 import CONSTANTS from "../../constants";
+import Spinner from "./Spinner";
 
 export default function Preview({
 	selectedFrames,
-	setSelectedFrames
+	onPreviewLoad
 }: {
 	selectedFrames: number[],
-	setSelectedFrames: (e: number[]) => void
+	onPreviewLoad: (e: number) => void
 }) {
 	const previewFps = CONSTANTS.previewFps;
 	const videoRef = useRef<HTMLVideoElement>(null) as React.MutableRefObject<HTMLVideoElement>;
-	const [totalFrames, setTotalFrames] = useState(0);
-	const [audioContext, setAudioContext] = useState<null | AudioContext>(null);
 
-	useEffect(() => {
-		const initializeAudioContext = () => {
-			// @ts-expect-error: webkitAudioContext is not in the lib but needed for iOS / Safari
-			const context = new (window.AudioContext || window.webkitAudioContext)();
-			setAudioContext(context);
-		};
-
-		initializeAudioContext();
-	}, []);
-
-	const playClickSound = () => {
-		if (!audioContext) return;
-
-		const buffer = audioContext.createBuffer(1, audioContext.sampleRate * 0.02, audioContext.sampleRate);
-		const data = buffer.getChannelData(0);
-
-		for (let i = 0; i < buffer.length; i++) {
-			data[i] = Math.random() * 2 - 1; // Generate random noise
-		}
-
-		const source = audioContext.createBufferSource();
-		source.buffer = buffer;
-
-		const filter = audioContext.createBiquadFilter();
-		filter.type = 'bandpass';
-		filter.frequency.value = 5000;
-
-		const gainNode = audioContext.createGain();
-		gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
-
-		source.connect(filter);
-		filter.connect(gainNode);
-		gainNode.connect(audioContext.destination);
-
-		source.start();
-		gainNode.gain.exponentialRampToValueAtTime(0.00001, audioContext.currentTime + 0.02);
-		source.stop(audioContext.currentTime + 0.02);
-	};
-
-
-	const handleInput = (e: number[]) => {
-
-		// check which handle is changing and update preview
-		if (e[0] !== selectedFrames[0]) {
-			videoRef.current.currentTime = e[0] / previewFps;
-		} else if (e[1] !== selectedFrames[1]) {
-			videoRef.current.currentTime = e[1] / previewFps;
-		}
-
-		// Play sound here
-		playClickSound();
-		setSelectedFrames(e)
-	};
+	const [videoLoaded, setVideoLoaded] = useState(false);
 
 	const handlePlayPause = () => {
 		if (videoRef.current.paused) {
@@ -77,72 +25,54 @@ export default function Preview({
 		}
 	}
 
+	// if (e[0] !== selectedFrames[0]) {
+	// 	videoRef.current.currentTime = e[0] / previewFps;
+	// } else if (e[1] !== selectedFrames[1]) {
+	// 	videoRef.current.currentTime = e[1] / previewFps;
+	// }
+
 	useEffect(() => {
-		// Extract preview video path from query params
-		const params = new URLSearchParams(window.location.search);
-		const previewPath = params.get("file");
-		const previewUrl = convertFileSrc(previewPath!);
+		// Wait for preview to be ready
+		const previewListener = listen("preview-ready", () => {
 
-		// Set the source to the preview URL
-		videoRef.current.src = previewUrl;
-		videoRef.current.load();
+			// Extract preview video path from query params
+			const params = new URLSearchParams(window.location.search);
+			const previewPath = params.get("file");
+			const previewUrl = convertFileSrc(previewPath!);
 
-		// Get duration (in seconds) after the video is loaded
-		videoRef.current.addEventListener('loadedmetadata', () => {
-			const duration = videoRef.current.duration;
-			const frames = Math.floor(duration * previewFps);
-			setTotalFrames(frames);
-			setSelectedFrames([0, frames]);
+			// Set the source to the preview URL
+			videoRef.current.src = previewUrl;
+			videoRef.current.load();
+
+			// Get duration (in seconds) after the video is loaded
+			videoRef.current.addEventListener('loadedmetadata', () => {
+				const duration = videoRef.current.duration;
+				const frames = Math.floor(duration * previewFps);
+
+				setVideoLoaded(true); // hide spinner
+				onPreviewLoad(frames); // pass total frames upwards
+			});
 		});
 
+		return () => {
+			previewListener.then(unlisten => unlisten())
+		}
 	}, []);
 
 	return <div className="w-[95%] h-fit rounded-xl overflow-hidden m-auto mt-0 mb-0 flex flex-col p-4 gap-2">
-		<video muted controls={false}
-			className="w-full object-cover rounded-md overflow-hidden h-[380px] border border-neutral-600" width={800} height={480} ref={videoRef}
-			onClick={handlePlayPause}
-		/>
-		<div className="w-full h-4 flex justify-between items-end">
-			{Array.from({ length: totalFrames }, (_, i) => {
+		{
+			videoLoaded
+				? <video
+					className="w-full object-cover rounded-md overflow-hidden h-[380px] border border-neutral-600"
+					onClick={handlePlayPause}
+					controls={false}
+					ref={videoRef}
+					height={480}
+					width={800}
+					muted
+				/>
+				: <Spinner />
+		}
 
-				let color = "bg-[rgba(255,255,255,0.3)]"
-
-				// if i is included in selected frames
-				if (i >= selectedFrames[0] && i <= selectedFrames[1]) {
-					color = "bg-[orange]"
-				}
-
-				if (i % previewFps === 0) {
-					return (
-						<div key={i} className={`w-[2px] h-full ${color}`} />
-					)
-				} else {
-					return (
-						<div key={i} className={`w-[2px] h-1/2 ${color}`} />
-					)
-				}
-			})}
-		</div>
-
-		<Slider.Root
-			min={0}
-			step={1}
-			max={totalFrames}
-			value={selectedFrames}
-			className="relative flex items-center select-none touch-none h-5"
-			onValueChange={(e) => handleInput(e)}
-		>
-			<Slider.Track className="bg-[transparent] relative grow rounded-full h-[3px] w-full">
-				<Slider.Range className="absolute bg-[transparent] rounded-full h-full" />
-			</Slider.Track>
-			<Slider.Thumb
-				className="block w-5 h-6 bg-[orange] rounded-2xl rounded-ss-3xl rounded-se-3xl translate-x-[-10px]"
-				aria-label="Start Frame"
-			/>
-			<Slider.Thumb
-				className="block w-5 h-6 bg-[orange] rounded-2xl rounded-ss-3xl rounded-se-3xl translate-x-[10px]"
-				aria-label="End Frame"
-			/>
-		</Slider.Root>
 	</div>
 }
