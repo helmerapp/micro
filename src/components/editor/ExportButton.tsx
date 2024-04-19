b9c743edfbd82d57b57feb938ee7120afc603bf0
clearlysid <siddharth99c@gmail.com>
2024-04-19 09:05:54 +0530

    feat: change cursor to wait when exporting

--- src/components/editor/ExportButton.tsx
+++ src/components/editor/ExportButton.tsx
@@ -10,7 +10,11 @@ export default function ExportButton({ clickHandler, exporting }: {
 			e.preventDefault();
 			if (!exporting) clickHandler()
 		}}
-		className="w-[280px] outline-none cursor-pointer bg-[orange] border-none px-6 py-4 m-0 inline-block relative uppercase tracking-wider font-bold text-sm rounded-xl overflow-hidden before:absolute before:inset-0 before:bg-[rgba(255,255,255,0.2)] before:blur-md before:skew-x-12 before:-translate-x-full before:transition-transform before:duration-300 before:ease-in-out hover:before:translate-x-full">
+		className="w-[280px] outline-none cursor-pointer bg-[orange] border-none px-6 py-4 m-0 inline-block relative uppercase tracking-wider font-bold text-sm rounded-xl overflow-hidden before:absolute before:inset-0 before:bg-[rgba(255,255,255,0.2)] before:blur-md before:skew-x-12 before:-translate-x-full before:transition-transform before:duration-300 before:ease-in-out hover:before:translate-x-full"
+		style={{
+			cursor: exporting ? "wait" : "pointer"
+		}}
+	>
 		{
 			exporting
 				? <Spinner size={20} />
