2db15e1ad4c4d4d4a7cb61a3302d92f1f1778488
clearlysid <siddharth99c@gmail.com>
2024-04-21 18:25:20 +0000

    feat: use windows native encoder instead of ffmpeg

--- src-tauri/src/recorder/utils/mod.rs
+++ src-tauri/src/recorder/utils/mod.rs
@@ -84,8 +84,51 @@ pub fn preview_encoder_thread_handler(
 ) {
     #[cfg(target_os = "windows")]
     {
+        use windows_capture::encoder::{VideoEncoder, VideoEncoderQuality, VideoEncoderType};
 
-        
+        let mut encoder = VideoEncoder::new(
+            VideoEncoderType::Mp4,
+            VideoEncoderQuality::HD1080p,
+            width as u32,
+            height as u32,
+            file_path,
+        )
+        .expect("Failed to create video encoder");
+
+        let mut start_time: u64 = 0;
+
+        // Process data until the channel is closed
+        while let Ok(data) = rx.recv() {
+            match data {
+                Frame::BGRA(frame) => {
+                    if start_time == 0 {
+                        start_time = frame.display_time;
+                    }
+
+                    let timespan_nanos =
+                        std::time::Duration::from_nanos(frame.display_time - start_time);
+
+                    // TODO: why does the magic number 10 work here?
+                    let timespan_micros = timespan_nanos.as_micros() as i64;
+                    let timespan_micros_10 = timespan_micros * 10;
+
+                    let buffer = flip_image_vertical_bgra(&frame.data, width, height);
+
+                    encoder
+                        .send_frame_buffer(&buffer, timespan_micros_10)
+                        .expect("failed to send frame");
+                }
+                _ => {}
+            }
+        }
+        println!("Recording stopped");
+        let x = encoder.finish();
+        match x {
+            Ok(_) => {
+                println!("Encoding complete");
+            }
+            Err(e) => println!("Error: {:?}", e),
+        }
     }
 
     #[cfg(target_os = "macos")]
@@ -116,8 +159,8 @@ pub fn preview_encoder_thread_handler(
             Err(e) => println!("Error: {:?}", e),
         }
         drop(encoder);
-        println!("Processing thread terminated.");
     }
+    println!("Processing thread terminated.");
 }
 
 pub fn get_random_id() -> String {
