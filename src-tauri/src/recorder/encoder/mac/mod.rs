4ace5551fcdbef18c658c663a0a691180b08b679
clearlysid <siddharth99c@gmail.com>
2024-04-22 23:03:36 +0530

    feat: adds swift-rust package

---
+++ src-tauri/src/recorder/encoder/mac/mod.rs
@@ -0,0 +1,21 @@
+use swift_rs::swift;
+pub use swift_rs::{Int, SRData, SRString};
+
+swift!(pub fn encoder_init(
+    width: Int,
+    height: Int,
+    out_file: SRString
+) -> *mut std::ffi::c_void);
+
+swift!(pub fn encoder_ingest_yuv_frame(
+    enc: *mut std::ffi::c_void,
+    width: Int,
+    height: Int,
+    display_time: Int,
+    luminance_stride: Int,
+    luminance_bytes: SRData,
+    chrominance_stride: Int,
+    chrominance_bytes: SRData
+));
+
+swift!(pub fn encoder_finish(enc: *mut std::ffi::c_void));
