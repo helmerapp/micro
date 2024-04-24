97ba20ade54233b5234c27799b6f706e7203006e
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-04-24 20:45:52 +0530

    fix: reset toolbar animation on stopping recording

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -46,10 +46,10 @@ const ToolbarReact = () => {
 					? `conic-gradient(white 360deg, lightseagreen 360deg, lightseagreen 360deg)`
 					: `conic-gradient(white 0deg, lightseagreen 0deg, lightseagreen 360deg)`
 			}}
-			transition={{
+			transition={recording ? {
 				duration: MAX_RECORDING_LIMIT_SECONDS,
 				ease: "linear"
-			}}
+			} : undefined}
 			onAnimationComplete={(e) => {
 				// TODO: refactor this to use framer motion variants
 				// @ts-expect-error: backgroundImage is not a valid property
