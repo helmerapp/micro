2889c1b711b2622974430d31c40b4c30261847d9
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-04-27 18:09:47 +0530

    fix: select all frames by default on opening editor

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { invoke } from '@tauri-apps/api/core'
 
 import CONSTANTS from '../../constants';
@@ -12,6 +12,10 @@ export default function Editor() {
 	const [exporting, setExporting] = useState(false);
 	const [totalFrames, setTotalFrames] = useState(0);
 	const [selectedFrames, setSelectedFrames] = useState([0, totalFrames]);
+
+	useEffect(() => {
+		setSelectedFrames([0, totalFrames]);
+	}, [totalFrames]);
 
 	const exportHandler = (options: {
 		fps: number,
