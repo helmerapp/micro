98307b64f6d51ab46089a4152850a91cd1931251
clearlysid <siddharth99c@gmail.com>
2024-04-29 21:49:52 +0530

    feat: send app version alongside all events

--- src/components/Posthog.tsx
+++ src/components/Posthog.tsx
@@ -1,6 +1,8 @@
 import { ReactElement, useEffect, useState } from "react";
-import { PostHogProvider } from "posthog-js/react"
+import { PostHogProvider, usePostHog } from "posthog-js/react"
 import { invoke } from "@tauri-apps/api/core";
+import { app } from "@tauri-apps/api";
+
 
 const posthogKey = import.meta.env.PUBLIC_POSTHOG_KEY;
 const posthogHost = import.meta.env.PUBLIC_POSTHOG_HOST;
@@ -8,6 +10,15 @@ const posthogHost = import.meta.env.PUBLIC_POSTHOG_HOST;
 export default function PosthogProvider({ children }: { children: ReactElement }) {
 
 	const [isOkSharingUsageData, setIsOkSharingUsageData] = useState(true);
+	const posthog = usePostHog();
+
+	useEffect(() => {
+		app.getVersion().then(version => {
+			posthog.register({
+				'App Version': version
+			})
+		})
+	}, [])
 
 	useEffect(() => {
 		invoke('is_ok_sharing_usage_data').then((res) => {
